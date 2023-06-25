9cd394b33898b89a6f8669efb21a3a947d5b924f
SongZihui <1751122876@qq.com>
2023-06-25 16:05:05 +0000

    feat(device): 设备层

--- js/device.ts
+++ js/device.ts
@@ -5,16 +5,94 @@
 //  date: 2023-6-25
 //
 
+/**
+ *  设备优先级
+ *  USER 用户级
+ *  INTERLAYER 中间层
+ *  CORE 内核层
+ */
 enum device_priority
 {
-    CORE,
-    USER,
-
+    USER = 1,
+    INTERLAYER = 2,
+    CORE = 3
 };
 
+/**
+ *  设备类型
+ *  SYSTEM 系统设备
+ *  USER    用户设备
+ *  STORAGE 存储设备
+ *  INPUT   输入设备
+ *  OUTPUT  输出设备
+ */
+enum device_type
+{
+    SYSTEM = "SYSTEM",
+    USER = "USER",
+    STORAGE = "STORAGE",
+    INPUT = "INPUT",
+    OUTPUT = "OUTPUT"
+};
+
+/**
+ *  设备缓冲区
+ */
+class device_buffer
+{
+    mBuffer: Array<any>;
+
+    constructor() {}
+
+    /**
+     * write
+     * @param data 添加的数据
+     * 写入数据
+     */
+    write(data: any)
+    {
+        this.mBuffer.push(data);
+    }
+
+    /**
+     * read
+     * @returns 最新的数据
+     * 读取队列头的数据
+     */
+    read()
+    {
+        let data: any = this.mBuffer[0];
+        this.mBuffer.pop();
+        return data;
+    }
+
+    /**
+     * clear
+     * 清空缓冲区
+     */
+    clear()
+    {
+        this.mBuffer.splice(0, this.mBuffer.length);
+    }
+};
+
+/**
+ *  设备对象
+ */
 class device
 {
-    constructor() 
+    mPriority: device_priority; /* 优先级 */
+    mName: string;  /* 设备名 */
+    mType: device_type; /* 设备类型 */
+
+    /* 构造函数 */
+    constructor(name: string, type: device_type, priority: device_priority) 
     {
+        this.mName = name;
+        this.mType = type;
+        this.mPriority = priority;
     }
+
 }
+
+export { device, device_buffer, device_type, device_priority };
--- js/mouse.ts
+++ js/mouse.ts
@@ -5,10 +5,59 @@
 //  date: 2023-6-25
 //
 
-class mouse
-{
-    constructor(parameters)
-    {
+import { device, device_buffer, device_type, device_priority } from "./device";
+import RightMenu from '@right-menu/core';
 
+class mouse extends device
+{
+    mBuffer: device_buffer;
+
+    /* 构造函数 */
+    constructor(name: string, type: device_type, priority: device_priority)
+    {
+        super(name, type, priority);
+        this.mBuffer = new device_buffer();
     }
-}
+
+    /**
+     * 设备鼠标右键菜单
+     * @param table 菜单项
+     */
+    right_menu(table: Array<string>)
+    {
+        const options: any = [{ type: 'li', text: '测试111' }];
+
+        new RightMenu({
+            el: '.demo1',
+            theme: 'win10'
+        }, options);
+    }
+
+    /**
+     *  鼠标点击事件
+     */
+    click(right_callback: Function, left_callback: Function, roller_callback: Function)
+    {
+        document.addEventListener("mousedown", (e) =>
+        {
+            if (e.button == 1) 
+            {
+                console.log("Roller click");
+                roller_callback();
+            }
+            else if (e.button == 2)
+            {
+                console.log("right click");
+                right_callback();
+            }
+            else
+            {
+                console.log("left click");
+                left_callback();
+            }
+            e.preventDefault();
+        });
+    }
+};
+
+export { mouse };
