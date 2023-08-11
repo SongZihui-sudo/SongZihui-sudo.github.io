19b6b90c45ab91dc7631930b6b5f4f85660be806
宋子慧 <1751122876@qq.com>
2023-08-11 23:12:06 +0800

    add: framework for website

---
+++ js/webpack.config.js
@@ -0,0 +1,20 @@
+"use strict";
+const path = require('path'); // node自带包
+module.exports = {
+    entry: './ts/main.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve('dist') //获取输出路径
+    },
+    mode: 'development',
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }]
+    },
+    resolve: {
+        extensions: ['.ts'] // 解析对文件格式
+    },
+};
--- ts/main.ts
+++
@@ -1,6 +0,0 @@
-import { device_type, device_priority } from "./device";
-import { device_manager } from "./device_manager";
-import { mouse } from "./mouse";
-import { global_registration_form } from "./registration_form";
-
-let gloal_device_manager: device_manager = new device_manager([new mouse("mouse", device_type.INPUT, device_priority.CORE)]);
